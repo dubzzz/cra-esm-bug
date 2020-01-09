e26d75755d69da7960d8891f6d65b3ffd6e87ff5
Nicolas DUBIEN <github@dubien.org>
2020-01-10 00:32:25 +0100

    Bug repro

--- basic-cra/src/App.test.js
+++ basic-cra/src/App.test.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("should render the same content as 'yarn start' does", () => {
+  const { container } = render(<App />);
+  expect(container).toMatchInlineSnapshot(`
+    <div>
+      esm
+    </div>
+  `);
 });
